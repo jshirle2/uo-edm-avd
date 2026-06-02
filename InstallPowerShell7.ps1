ad5caf13e6367943ad881e9a028a3f272d764ecb
Jason Shirley <jshirle2@uoregon.edu>
2026-06-02 08:50:13 -0700

    Added logging to pwsh install

--- InstallPowerShell7.ps1
+++ InstallPowerShell7.ps1
@@ -1,6 +1,29 @@
 $url = 'https://github.com/PowerShell/PowerShell/releases/download/v7.6.2/PowerShell-7.6.2-win-x64.msi'
 $path = 'C:\Temp\pwsh.msi'
 
+$LogPath = 'C:\UO\logs'
+$LogFile = "$LogPath\Powershell-Install.log"
+
+function Log {
+    param ($Message)
+
+    if (-not (Test-Path $LogPath)) {
+        New-Item -Path $LogPath -ItemType Directory -Force | Out-Null
+    }
+
+    $timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+    "$timestamp  $Message" | Tee-Object -FilePath $LogFile -Append
+    Write-Host $Message
+}
+
+# Delete previous log
+if (Test-Path $LogFile) {
+    Remove-Item $LogFile -Force
+}
+
+Log "Installing NuGet provider."
+
+
 If (-Not (Test-Path -Path $path)) {
     New-Item -ItemType File -Path $path -Force | Out-Null
 }
